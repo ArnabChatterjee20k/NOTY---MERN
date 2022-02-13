4d3f0af2dff79a54fd39712b5abe1f8233050ca7
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-13 20:05:04 +0530

    added jwt auth signing to the responses

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const User = require("../models/User");
-const router = express.Router()
+const router = express.Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require("jsonwebtoken");
 const { body, validationResult } = require('express-validator');
 
 // validations and custom messages for submitting user data
@@ -11,6 +11,9 @@ const validation_checks = [
     body("name", "Enter valid name").isLength({ min: 3 }),
     body("password", "Enter valid password").isLength({ min: 5 }),
 ]
+
+// secret for jwt token
+const JWT_SECRET = ";adsjfldasjfldgjlkdgfdklglkfjglueouroejrlejrlenf,dnf,"
 
 // Creating a user. POST "/api/auth/createuser". No auth required
 router.post("/createuser", validation_checks,
@@ -24,8 +27,8 @@ router.post("/createuser", validation_checks,
 
         // Checking existance of user
         try {
-            let user = await User.findOne({ email: req.body.email });
-            if (user) {
+            let req_user = await User.findOne({ email: req.body.email });
+            if (req_user) {
                 return res.status(400).json({ errors: "User already exists" });
             }
             // If user not exists then do this
@@ -34,16 +37,21 @@ router.post("/createuser", validation_checks,
             const salt = await bcrypt.genSalt(10);
             const secPass = await bcrypt.hash(req.body.password , salt);
 
-            await User.create({
+            const new_user = await User.create({
                 name: req.body.name,
                 password: secPass,
                 email: req.body.email
             })
-            const response = {
-                status: 'done',
-                credentials: req.body
+            
+            // data for jwt token
+            const data = {
+                id : new_user.id
             }
-            res.json(response)
+            
+            // signing the token
+            const authtoken = jwt.sign(data , JWT_SECRET)
+            console.log({authtoken})
+            res.json({authtoken})
         } catch (error) {
             res.status(500).json({"error":"some problem occured..."})
             console.log(error)
