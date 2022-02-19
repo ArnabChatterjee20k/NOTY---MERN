a9f536fc6c69f578cf51ef8e6fffd1220c4eb1dc
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-02-19 19:31:57 +0530

    setupped react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <div className="App">
-      <h1>Noty Frontend</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          
+          <Route path="/about" exact>
+            <About />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
